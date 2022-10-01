5bd56210a0a9f2e7b1f1dde15af0c907d323c8d6
MANRED12 <roral@hotmail.com>
2022-10-01 17:25:21 -0500

    first commit

---
+++ src/test/java/com/team7/ComputerCenter/ComputerCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team7.ComputerCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
